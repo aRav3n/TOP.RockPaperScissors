2b996e9b1dc847ae687ce53e6ba902a2984a067f
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-01-20 08:53:54 +0530

    Structure the game

--- script.js
+++ script.js
@@ -5,4 +5,18 @@ let computerThrow;
 let userScore = 0;
 let computerScore = 0;
 
-// 
+// Get computer choice
+
+// Get player choice
+
+// Convert player choice to lower case
+
+// Validate player choice
+
+// Compare computer selection vs player selection
+
+// Determine the winner
+
+// Update scores
+
+// Return the winner
