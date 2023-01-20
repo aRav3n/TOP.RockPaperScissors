b40157f9da320b4d959613a3b2ee5d14ff1ae1c5
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-01-20 16:18:23 +0530

    Add function to determine if the player or computer won

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 // Initialize variables
-let result;
-let userThrow;
-let computerThrow;
-let possibleThrows = ['rock','paper','scissors']
-let userScore = 0;
+const possibleThrows = ['rock','paper','scissors'];
+let playerScore = 0;
 let computerScore = 0;
 
 // Get computer choice
@@ -14,16 +11,37 @@ function getComputerChoice () {
 };
 
 // Get player choice
+function getPlayerChoice () {
+    let throwWord = prompt('Would you like to throw rock, paper, or scissors?');
+    // Convert player choice to lower case
+    throwWord = throwWord.toLowerCase();
 
+    // Validate player choice
+    if (possibleThrows.includes(throwWord)) {
+        return throwWord;
+    } else {
+        return throwWord + ' is not a legitimate choice. Please try again.';
+    };
+};
 
-// Convert player choice to lower case
+// Compare computerThrow vs playerThrow to determine the winner
+function compareThrows (playerSelection, computerSelection) {
+    // Evaluate for tie
+    if (playerSelection === computerSelection){
+        return 'Tie game!'
+    }
+    // Evaluate for computer win and update score
+    else if (computerSelection === 'rock' && playerSelection === 'scissors' || 
+            computerSelection === 'paper' && playerSelection === 'rock' ||
+            computerSelection === 'scissors' && playerSelection === 'paper') {
+        computerScore++;
+        return 'You lose; ' + computerSelection + ' beats ' + playerSelection + '!';
+    }
+    // Evaluate for player win and update score
+    else {
+        playerScore++;
+        return 'You win; ' + playerSelection + ' beats ' + computerSelection + '!'
+    }
+};
 
-// Validate player choice
-
-// Compare computer selection vs player selection
-
-// Determine the winner
-
-// Update scores
-
-// Return the winner
+// Run 5 rounds of the game
