58a0232e0edc2d258d23813df8a5365177868e61
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-01-20 08:26:50 +0530

    Convert actions to functions for modularity
    Upon reading the instructions it was realized that the assignment
    was executed incorrectly. Begin fixing the program.

--- script.js
+++ script.js
@@ -1,49 +1,63 @@
 // Initialize result variable
+let result;
 
-// Get input from user (rock, paper, or scissors)
-let userThrow = prompt("Would you like to throw rock, paper, or scissors?");
-userThrow = userThrow.toLocaleLowerCase();
+function userChoice() {
+    // Get input from user (rock, paper, or scissors)
+    let userThrow = prompt("Would you like to throw rock, paper, or scissors?");
+    userThrow = userThrow.toLocaleLowerCase();
 
-// Validate the user input
-if (userThrow !== "rock" || userThrow !== "paper" || userThrow !== "scissors"){
-    console.log();
-    result = "You typed " + userThrow + ". That's not a valid throw; please double check your spelling and try again.";
-}
+    // Validate the user input
+    if (userThrow !== "rock" || userThrow !== "paper" || userThrow !== "scissors"){
+        console.log();
+        return result = "You typed " + userThrow + ". That's not a valid throw; please double check your spelling and try again.";
+    };
 
-// Use random number generator to generate number 0-2
-let computerThrow = Math.floor(Math.random() * 3);
+    // Return choice
+    return userThrow;
+};
 
-// Assign numbers to a value (rock, paper, or scissors)
-if (computerThrow === 0){
-    computerThrow = "rock"
-} else if (computerThrow === 1) {
-    computerThrow = "paper"
-} else {
-    computerThrow = "scissors"
+function computerChoice (){
+    // Use random number generator to generate number 0-2
+    let computerThrow = Math.floor(Math.random() * 3);
+
+    // Assign numbers to a value (rock, paper, or scissors)
+    if (computerThrow === 0){
+        computerThrow = "rock"
+    } else if (computerThrow === 1) {
+        computerThrow = "paper"
+    } else {
+        computerThrow = "scissors"
+    };
+
+    // Return choice
+    return computerThrow;
 };
 
 // Find the winner
-if (computerThrow === userThrow) {
-    result = "Tie game! You threw " + userThrow + " and the computer threw " + computerThrow + ". Better luck next time!";
-}
-else if (computerThrow === "rock" && userThrow === "scissors") {
-    result = "The computer won. You threw " + userThrow + " and the computer threw " + computerThrow + ". Better luck next time!";
-}
-else if (computerThrow === "paper" && userThrow === "rock") {
-    result = "The computer won. You threw " + userThrow + " and the computer threw " + computerThrow + ". Better luck next time!";
-}
-else if (computerThrow === "scissors" && userThrow === "paper") {
-    result = "The computer won. You threw " + userThrow + " and the computer threw " + computerThrow + ". Better luck next time!";
-}
-else if (computerThrow === "rock" && userThrow === "paper") {
-    result = "You won! You threw " + userThrow + " and the computer threw " + computerThrow + "!";
-}
-else if (computerThrow === "paper" && userThrow === "scissors") {
-    result = "You won! You threw " + userThrow + " and the computer threw " + computerThrow + "!";
-}
-else if (computerThrow === "scissors" && userThrow === "rock") {
-    result = "You won! You threw " + userThrow + " and the computer threw " + computerThrow + "!";
-};
+function playRound (userThrow, computerThrow)
+{
+    if (computerThrow === userThrow) {
+        result = "Tie game! You threw " + userThrow + " and the computer threw " + computerThrow + ". Better luck next time!";
+    }
+    else if (computerThrow === "rock" && userThrow === "scissors") {
+        result = "The computer won. You threw " + userThrow + " and the computer threw " + computerThrow + ". Better luck next time!";
+    }
+    else if (computerThrow === "paper" && userThrow === "rock") {
+        result = "The computer won. You threw " + userThrow + " and the computer threw " + computerThrow + ". Better luck next time!";
+    }
+    else if (computerThrow === "scissors" && userThrow === "paper") {
+        result = "The computer won. You threw " + userThrow + " and the computer threw " + computerThrow + ". Better luck next time!";
+    }
+    else if (computerThrow === "rock" && userThrow === "paper") {
+        result = "You won! You threw " + userThrow + " and the computer threw " + computerThrow + "!";
+    }
+    else if (computerThrow === "paper" && userThrow === "scissors") {
+        result = "You won! You threw " + userThrow + " and the computer threw " + computerThrow + "!";
+    }
+    else if (computerThrow === "scissors" && userThrow === "rock") {
+        result = "You won! You threw " + userThrow + " and the computer threw " + computerThrow + "!";
+    };
 
-// Output the winner
-console.log(result);
+    // Return the winner
+    return result;
+};
