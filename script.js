02cd50f0382106389709c1938fa8f584c41e8de3
aRav3n <24660772+aRav3n@users.noreply.github.com>
2023-01-20 09:06:38 +0530

    Select random choice for computer

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 let result;
 let userThrow;
 let computerThrow;
+let possibleThrows = ['rock','paper','scissors']
 let userScore = 0;
 let computerScore = 0;
 
 // Get computer choice
+function getComputerChoice () {
+    let throwNumber = Math.floor(Math.random() * possibleThrows.length);
+    let throwWord = possibleThrows[throwNumber];
+    return throwWord;
+};
 
 // Get player choice
+
 
 // Convert player choice to lower case
 
